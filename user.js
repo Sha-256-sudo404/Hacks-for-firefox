061c1ef06aa5256a5107047731f8f3859b4d6065
Sha-256-sudo404 <179568043+Sha-256-sudo404@users.noreply.github.com>
2024-11-10 16:36:19 +0800

    Add files via upload

---
+++ user.js
@@ -0,0 +1,18 @@
+//cacheonram
+user_pref("browser.cache.disk.enable",false);
+user_pref("browser.cache.disk.capacity",0);
+user_pref("browser.cache.memory.enable",true);
+user_pref("browser.cache.memory.capacity",512000);
+user_pref("browser.sessionhistory.max_total_viewers", 4);
+user_pref("browser.cache.memory.capacity",1048576);
+
+//scrolling
+user_pref("general.smoothScroll",true);
+user_pref("general.smoothScroll.msdPhysics.enabled",true);
+
+//privacy
+user_pref("network.captive-portal-service.enabled",false);
+
+//rendering
+user_pref("media.ffmpeg.vaapi.enabled",true);
+user_pref("browser.preferences.defaultPerformanceSettings.enabled",false);

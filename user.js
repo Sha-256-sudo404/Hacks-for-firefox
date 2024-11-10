327430a40bbb67121ac309df0b53100241e74932
Sha-256-sudo404 <179568043+Sha-256-sudo404@users.noreply.github.com>
2024-11-10 16:59:44 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,17 +1,22 @@
-//cacheonram: to reduce i/o on ssd and make better use of ram without the fuss of creating ramdisk
+//cacheonram: to reduce i/o on SSD and make better use of ram without the fuss of creating ramdisk, ENSURE RAM IS NOT THE BOTTLENECK BEFORE USAGE
 user_pref("browser.cache.disk.enable",false);
 user_pref("browser.cache.disk.capacity",0);
 user_pref("browser.cache.memory.enable",true);
-user_pref("browser.cache.memory.capacity",512000); //512MB, can change value < half of all ram, otherwise is bloat and waste of ram
-user_pref("browser.sessionhistory.max_total_viewers", 4);// sane No. of sessions stored per tab
+user_pref("browser.cache.memory.capacity",512000); //512MB, should change value < quater of all ram, otherwise is bloat and waste of ram
+user_pref("browser.sessionhistory.max_total_viewers", 4);// sane No. of sessions stored per tab, to reduce cache accumulation
 
-//scrolling
+//scrolling for better visuals, only recommended for refresh rates 60Hz<#<75Hz,otherwise checkout smoothfox
 user_pref("general.smoothScroll",true);
 user_pref("general.smoothScroll.msdPhysics.enabled",true);
 
-//privacy
+//privacy: remove extra pinging to servers for checking internet connectivity
 user_pref("network.captive-portal-service.enabled",false);
+//Additional tweaks
+//Lv.1 Disable sessin restore, search suggestions
+//Lv.2 Ublock origin installed + strict mode for Enhanced Tracking Protection, Clear browsing history on exit for those who don't bother login everytime, HTTPS only, DNS-over-HTTPS (1.1.1.1, Rethinkdns...)
+//Lv.3 arkenfox user.js recommendations or directly use librewolf for max privcay
 
-//rendering
+
+//rendering tweaks: hardware acceleration tweaks for power efficiency
 user_pref("media.ffmpeg.vaapi.enabled",true);
 user_pref("browser.preferences.defaultPerformanceSettings.enabled",false);
